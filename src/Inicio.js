4042f004f7d4ff9f8e5239672e82b4092a4eca51
Erick Jordan <erick_jstm@hotmail.com>
2022-10-28 18:06:22 -0300

    AlteracaoDimensaoConvite

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import logo from './Convite.jpeg';
+import logo from './Convite.png';
 import './App.css';
 
-function Ini(){
-    
-       
-        return (
-            <div className="App">              
-            <header className="App-header">     
-              <img src={logo} className="App-logo" alt="logo" />
-            </header>
-          </div>
+function Ini() {
 
-        )
-        
-    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+
+  )
+
+}
 
 export default Ini;
